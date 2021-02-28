4643b7c98d3be403e0e9b0f0f87809bd2b48c6af
clement <lomn.contact@gmail.com>
2021-02-28 19:06:48 +0100

    optimisation du if

--- src/Model/Model_CPU/Model_CPU_fast/Model_CPU_fast.cpp
+++ src/Model/Model_CPU/Model_CPU_fast/Model_CPU_fast.cpp
@@ -6,6 +6,7 @@
 
 #include <mipp.h>
 #include <omp.h>
+#include <math.h>
 
 Model_CPU_fast
 ::Model_CPU_fast(const Initstate& initstate, Particles& particles)
@@ -16,9 +17,53 @@ Model_CPU_fast
 void Model_CPU_fast
 ::step()
 {
-    std::fill(accelerationsx.begin(), accelerationsx.end(), 0);
-    std::fill(accelerationsy.begin(), accelerationsy.end(), 0);
-    std::fill(accelerationsz.begin(), accelerationsz.end(), 0);
+	std::fill(accelerationsx.begin(), accelerationsx.end(), 0);
+	std::fill(accelerationsy.begin(), accelerationsy.end(), 0);
+	std::fill(accelerationsz.begin(), accelerationsz.end(), 0);
+	
+	#pragma omp parallel for
+	for (int i = 0; i < n_particles; i++)
+	{
+		for (int j = 0; j < n_particles; j++)
+		{
+			if(i != j)
+			{
+				const float diffx = particles.x[j] - particles.x[i];
+				const float diffy = particles.y[j] - particles.y[i];
+				const float diffz = particles.z[j] - particles.z[i];
+
+				float dij = diffx * diffx + diffy * diffy + diffz * diffz;
+
+                //float dijcond = static_cast<float>(signbit(dij-1))-0.5;
+                //dij = (1.0+dijcond)*10.0/(dij*std::sqrt(dij)) + (1.0-dijcond)*10.0;
+
+                if (dij < 1.0)
+				{
+					dij = 10.0;
+				}
+				else
+				{
+					dij = 10.0 / (dij * std::sqrt(dij));
+				}
+
+				accelerationsx[i] += diffx * dij * initstate.masses[j];
+				accelerationsy[i] += diffy * dij * initstate.masses[j];
+				accelerationsz[i] += diffz * dij * initstate.masses[j];
+			}
+		}
+	}
+	
+	#pragma omp parallel for
+	for (int i = 0; i < n_particles; i++)
+	{
+		velocitiesx[i] += accelerationsx[i] * 2.0f;
+		velocitiesy[i] += accelerationsy[i] * 2.0f;
+		velocitiesz[i] += accelerationsz[i] * 2.0f;
+		particles.x[i] += velocitiesx   [i] * 0.1f;
+		particles.y[i] += velocitiesy   [i] * 0.1f;
+		particles.z[i] += velocitiesz   [i] * 0.1f;
+	}
+
 
 // OMP  version
 // #pragma omp parallel for
